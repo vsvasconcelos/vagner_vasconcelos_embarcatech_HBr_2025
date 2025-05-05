0b85ad051894703b69c8b74cf0c748af0bab9d21
Vagner SV <vsvasconcelos@gmail.com>
2025-05-05 17:29:31 -0300

    Ajustado CMakeLists.txt do projeto Gaton Board

---
+++ projetos/galton_board/buid/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/vagner/pico/vagner_vasconcelos_embarcatech_HBr_2025/projetos/galton_board/buid/_deps/picotool/"
+};
